4858bf185a22cc40ed7d7504fb1cf6d2d8a9f4aa
Weiting <wei.chuang@seattlecentral.edu>
2018-10-29 20:47:47 -0700

    Added a few command in terminal

--- my_script.sh
+++ my_script.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 echo "hello ITC-136"
 echo "Edited in github"
+echo "This is your current working directory:"
+pwd
+
+echo "This is the version of linux you're running:"
+uname -a
+
+echo "These are your logged in users:"
+who
