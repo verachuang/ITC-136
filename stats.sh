2584ca39cefdbb7f6fc724419109c2f539a4f6d3
verachuang <33468819+verachuang@users.noreply.github.com>
2018-11-19 20:05:26 -0800

    Update stats.sh

--- stats.sh
+++ stats.sh
@@ -1,1 +1,25 @@
 #!/bin/bash
+
+diskused=$( df -h | grep "/dev/sda1" | awk '{print $5}')
+
+memfree=$( free -mh | grep Mem: | awk ‘{print $7}’ )
+
+connections=$( netstat | grep tcp )
+
+connections=$( netstat | grep tcp )
+
+user=$( who )
+
+echo "This is a snapshot of your current system:
+  Disk Used: $diskused
+  Free Memory: $memfree
+  Logged in Users: $user
+  Open Internet Connections: $connection"
+  
+echo "
+color info:"
+
+
+
+
+
