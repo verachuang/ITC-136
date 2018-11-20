80f531a705ae083f05a14d28b14dab3371e5ac3e
verachuang <wei.chuang@seattlecentral.edu>
2018-11-19 20:49:33 -0800

    added colors

--- stats.sh
+++ stats.sh
@@ -2,7 +2,7 @@
 
 diskused=$( df -h | grep "/dev/sda1" | awk '{print $5}')
 
-memfree=$( free -mh | grep Mem: | awk ‘{print $7}’ )
+memfree=$( free -mh | grep Mem: | awk '{print $7}' )
 
 connections=$( netstat | grep tcp )
 
@@ -10,16 +10,10 @@ connections=$( netstat | grep tcp )
 
 user=$( who )
 
-echo "This is a snapshot of your current system:
-  Disk Used: $diskused
-  Free Memory: $memfree
-  Logged in Users: $user
-  Open Internet Connections: $connection"
-  
-echo "
-color info:"
+echo -en "This is a snapshot of your current system:
+	\e[36m Disk Used: $diskused
+	\e[95m Free Memory: $memfree
+	\e[31m Logged in Users: $user
 
-
-
-
-
+\e[46m Open Internet Connections:
+$connection"
