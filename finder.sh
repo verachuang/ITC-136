783d12984727ff6167be7db5f5d478e2085c97eb
verachuang <33468819+verachuang@users.noreply.github.com>
2018-11-26 20:03:57 -0800

    Update finder.sh
    Added if and else

--- finder.sh
+++ finder.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+printf "\n Please enter a file name"
+read num3
+printf "\n Please enter the path to check"
+read path2check
+
+if find $path2check -name $num3 -print -quit | grep -q '^'; then
+  echo
+  echo "The file exists!"
+  echo
+  echo "Below are the locations of said filename copies..."
+  echo
+  ## use 2> /dev/null to get rid of pesky stderr
+  ##Permission denied... UCK...
+  find $pasth2check 2> /dev/null | grrp "\/$num3$" | less
+
+else
+  echo "The file does not exist!"
