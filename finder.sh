dde2a26a8e24fe387f70849d5f4971790afc3266
verachuang <wei.chuang@seattlecentral.edu>
2018-11-26 20:17:52 -0800

    Added details in finder.sh

--- finder.sh
+++ finder.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-printf "\n Please enter a file name"
+printf "\n Please enter a file name: "
 read num3
-printf "\n Please enter the path to check"
+printf "\n Please enter the path to check: "
 read path2check
 
 if find $path2check -name $num3 -print -quit | grep -q '^'; then
@@ -11,8 +11,9 @@ if find $path2check -name $num3 -print -quit | grep -q '^'; then
   echo "Below are the locations of said filename copies..."
   echo
   ## use 2> /dev/null to get rid of pesky stderr
-  ##Permission denied... UCK...
+  ## Permission denied...
   find $pasth2check 2> /dev/null | grrp "\/$num3$" | less
 
 else
   echo "The file does not exist!"
+fi
